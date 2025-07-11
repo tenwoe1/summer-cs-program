8293ebd3263e1d0f28fd44e23a4ba1a6f12db56f
Tenzin Dongtoekhang <tenzind80@nycstudents.net>
2025-07-11 14:03:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,12 @@
     
     map.fitBounds(pathLine.getBounds());
 
+    coords: [29.653, 91.117],
+    title: 'Lhasa',
+    content: `<h2>Lhasa</h2>
+                  <p>This is where my grandmother was born. She lived here until she left Tibet in 1959.</p>
+                  <img src="images/lhasa.jpg" alt="Lhasa">`
+
     L.marker(tracePoints[0]).addTo(map).bindPopup('Lhasa');
     L.marker(tracePoints[1]).addTo(map).bindPopup('Kathmandu');
     L.marker(tracePoints[2]).addTo(map).bindPopup('Mundgod');
